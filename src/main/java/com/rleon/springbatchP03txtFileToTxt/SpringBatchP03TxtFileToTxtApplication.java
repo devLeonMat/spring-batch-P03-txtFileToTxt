782a2e3f3efcfd3e1f8dec40488997137222bbad
leon matias roslin <leonmatias1991@gmail.com>
2021-02-25 11:48:21 -0500

    add basic project

---
+++ src/main/java/com/rleon/springbatchP03txtFileToTxt/SpringBatchP03TxtFileToTxtApplication.java
@@ -0,0 +1,13 @@
+package com.rleon.springbatchP03txtFileToTxt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchP03TxtFileToTxtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchP03TxtFileToTxtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rleon/springbatchP03txtFileToTxt/SpringBatchP03TxtFileToTxtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rleon.springbatchP03txtFileToTxt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchP03TxtFileToTxtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
